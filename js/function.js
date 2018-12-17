640a6b467eccf33462ee0ccc0ca0b9ebb05f9e2d
Picus85 <sergiypiknevych@gmail.com>
2018-12-17 14:40:16 +0000

    new

--- js/function.js
+++ js/function.js
@@ -1,4 +1,4 @@
-function data_run() {
+function quadraticEquation() {
   var a = document.data.a.value;
   var b = document.data.b.value;
   var c = document.data.c.value;
@@ -6,23 +6,24 @@ function data_run() {
   var d = b * b - 4 * a * c; // визначаю дискримінант
   var firstX;
   var secondX;
-  var rezultat;
+  var result;
 
   if (d < 0) {
-    rezultat = "Дискримінант < 0. Дійсних коренів нема.";
+    result = "Дискримінант < 0. Дійсних коренів нема.";
   }
   else {
-    if (d == 0) {
+    if (d === 0) {
 
       firstX = secondX = -b / (2 * a);
-      rezultat = "Корінь один (два однакових корені), <br>x<sub>1</sub> = x<sub>2</sub> = " + firstX;
+      result = "Корінь один (два однакових корені), <br>x<sub>1</sub> = x<sub>2</sub> = " + firstX;
     }
     else {
-      firstX = -b / (2 * a) - Math.sqrt(d) / (2 * a);
-      secondX = -b / (2 * a) + Math.sqrt(d) / (2 * a);
-      rezultat = "Два різні корені:<br>x<sub>1</sub> = " + firstX + ", <br>x<sub>2</sub> = " + secondX;
+      firstX = (-b - Math.sqrt(d)) / (2 * a);
+      secondX = (-b + Math.sqrt(d)) / (2 * a);
+      result = "Два різні корені:<br>x<sub>1</sub> = " + firstX + ", <br>x<sub>2</sub> = " + secondX;
     }
   }
-  real.innerHTML = rezultat;
+  real.innerHTML = result;
+
 
 }
